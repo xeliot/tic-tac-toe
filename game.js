311e07f270d77c3b56d07bccd005e8f82ffb61dd
xeliot <daveho8888@gmail.com>
2017-05-28 21:12:41 -0700

    create bDisabled array to check whether button can be clicked

--- game.js
+++ game.js
@@ -4,3 +4,5 @@ for(var i=1; i<10; i++) button[i] = document.getElementById('canvas'+i);
 var ctx = []; //stores the context of the canvases
 for(var i=1; i<10; i++) ctx[i] = button[i].getContext();
 
+var bDisabled = []; //stores the availability of the button
+for(var i=1; i<10; i++) bDisabled[i] = false; //all buttons are enabled in the beginning
