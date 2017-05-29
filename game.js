5d8270a89e777a746e80ce037cd08a16efc32f74
xeliot <daveho8888@gmail.com>
2017-05-28 21:11:19 -0700

    create ctx array to store the context of the canvases

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 var button = []; //stores the canvases
-
 for(var i=1; i<10; i++) button[i] = document.getElementById('canvas'+i);
 
+var ctx = []; //stores the context of the canvases
+for(var i=1; i<10; i++) ctx[i] = button[i].getContext();
 
