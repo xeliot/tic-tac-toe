7199f66ec25431f8d0d91fb21303798a9acc65e5
xeliot <daveho8888@gmail.com>
2017-05-28 21:17:50 -0700

    initialize loop function for button press

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var button = []; //stores the canvases
 for(var i=1; i<10; i++) button[i] = document.getElementById('canvas'+i);
 
 var ctx = []; //stores the context of the canvases
-for(var i=1; i<10; i++) ctx[i] = button[i].getContext();
+for(var i=1; i<10; i++) ctx[i] = button[i].msGetInputContext;
 
 var bDisabled = []; //stores the availability of the button
 for(var i=1; i<10; i++) bDisabled[i] = false; //all buttons are enabled in the beginning
@@ -10,3 +10,10 @@ for(var i=1; i<10; i++) bDisabled[i] = false; //all buttons are enabled in the b
 var isResult = false;
 var content = [];
 
+function loop(x)
+{
+    if(!bDisabled[x]){ //button does not currently contain X or O and therefore is enabled.
+        bDisabled[x] = true; //button now contains something
+        console.log("Button pressed.");
+    }
+}
