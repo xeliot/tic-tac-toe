da6982c0bfd8f9059606106924365b6575e80f9a
xeliot <daveho8888@gmail.com>
2017-05-31 21:56:50 -0700

    skeleton of splash screen

---
+++ splash.js
@@ -0,0 +1,17 @@
+(function() {
+    var preload = document.getElementById("preload");
+    var loading = 0;
+    var id = setInterval(frame, 64);
+
+    function frame() {
+        if(loading==60){
+            clearInterval(id);
+            window.open("tictactoe", "_self");
+        }else{
+            loading = loading + 1;
+            if(loading == 50) {
+                preload.style.animation = "fadeout 1s ease";
+            }
+        }
+    }
+})();
