610490258c3440cfa156c1982a5921fce2de730a
xeliot <daveho8888@gmail.com>
2017-05-31 22:04:55 -0700

    small changes

--- splash.js
+++ splash.js
@@ -4,12 +4,12 @@
     var id = setInterval(frame, 64);
 
     function frame() {
-        if(loading==60){
+        if(loading==15){
             clearInterval(id);
             window.open("tictactoe", "_self");
         }else{
             loading = loading + 1;
-            if(loading == 55) {
+            if(loading == 10) {
                 preload.style.animation = "fadeout 1s ease";
             }
         }
